d46d82e3c77e3931040aef6d4ae490346822f21d
raagan-u <n0r313n7@protonmail.ch>
2025-12-16 18:26:09 +0530

    small proxy

--- upgradeable_contracts/src/Counter.sol
+++ upgradeable_contracts/src/Counter.sol
@@ -1,14 +1,51 @@
-// SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.13;
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.28;
 
-contract Counter {
-    uint256 public number;
+import "lib/openzeppelin-contracts/contracts/proxy/Proxy.sol";
 
-    function setNumber(uint256 newNumber) public {
-        number = newNumber;
+contract SmallProxy is Proxy {
+    bytes32 private constant _IMPLEMENTATION_SLOT =
+        0x360894a13ba1a3210667c828492db98dca3e2076cc3735a920a3ca505d382bbc;
+
+    function setImplementation(address newImplementation) public {
+        assembly {
+            sstore(_IMPLEMENTATION_SLOT, newImplementation)
+        }
     }
 
-    function increment() public {
-        number++;
+    function _implementation()
+        internal
+        view
+        override
+        returns (address implementationAddress)
+    {
+        assembly {
+            implementationAddress := sload(_IMPLEMENTATION_SLOT)
+        }
+    }
+
+    function getDataToTransact(uint256 numToUpdate) public pure returns (bytes memory) {
+        return abi.encodeWithSignature("setValue(uint256", numToUpdate);
+    }
+
+    function readStorage() public view returns (uint256 valueAtStorageSlotZero) {
+        assembly {
+            valueAtStorageSlotZero := sload(0)
+        }
+    }
+}
+
+// When SmallProxy is deployed, it will use the ImplementationA contract as the implementation.
+// Any call that is made to SmallProxy will be delegated to the ImplementationA contract.
+
+contract ImplementationA {
+    uint256 public value;
+
+    function setValue(uint256 _value) public {
+        value = _value;
+    }
+
+    function getValue() public view returns (uint256) {
+        return value;
     }
 }
